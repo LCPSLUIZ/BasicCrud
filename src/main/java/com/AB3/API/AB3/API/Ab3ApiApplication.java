246fc1431312b0767b2c2f449e24c0d0c9f3ae3e
Luiz Santos <luqclaud@amazon.com>
2023-03-10 15:56:03 -0300

    Initial configurations (Database connection)

---
+++ src/main/java/com/AB3/API/AB3/API/Ab3ApiApplication.java
@@ -0,0 +1,13 @@
+package com.AB3.API.AB3.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ab3ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ab3ApiApplication.class, args);
+	}
+
+}
