4edfe740c3d38f12f079f706bb2d9ffca2b062ef
Luiz Santos <luqclaud@amazon.com>
2023-03-14 18:48:43 -0300

    Change request path

--- src/main/java/com/AB3/API/AB3/API/Controller/UserController.java
+++ src/main/java/com/AB3/API/AB3/API/Controller/UserController.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/basic-api")
+@RequestMapping("/")
 @CrossOrigin(origins = "*")
 public class UserController {
 
